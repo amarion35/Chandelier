07e7e3bd3cb6a5e6ad2f4bbc4e2f1e0bb0f26d84
amarion35 <alexis.marion.8@hotmail.fr>
2019-09-20 16:57:12 +0200

    Fix boolean means in metrics

--- chandelier/metrics.py
+++ chandelier/metrics.py
@@ -4,7 +4,7 @@ def binary_accuracy(y_true, y_pred):
     return torch.eq(torch.ge(y_pred, 0.5), y_pred.type(torch.bool)).type(torch.float).mean()
 
 def categorical_accuracy(y_true, y_pred):
-    return torch.eq(y_true.argmax(dim=-1), y_pred.argmax(dim=-1)).mean()
+    return torch.eq(y_true.argmax(dim=-1), y_pred.argmax(dim=-1)).type(torch.float).mean()
 
 def sparse_categorical_accuracy(y_true, y_pred):
-    return torch.eq(y_true, y_pred.argmax(dim=-1)).mean()
+    return torch.eq(y_true, y_pred.argmax(dim=-1)).type(torch.float).mean()
